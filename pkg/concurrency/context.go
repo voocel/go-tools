b32ac35d931d42c972dc4a512445805df505d6fa
voocel <voocel@gmail.com>
2022-12-06 23:37:24 +0800

    wip: add concurrency

---
+++ pkg/concurrency/context.go
@@ -0,0 +1,43 @@
+package concurrency
+
+import (
+	"context"
+	"time"
+)
+
+// closedctx is an always closed context.
+var closedctx = func() context.Context {
+	ctx := make(cancelctx)
+	close(ctx)
+	return ctx
+}()
+
+// ContextWithCancel returns a new context.Context impl with cancel.
+func ContextWithCancel() (context.Context, context.CancelFunc) {
+	ctx := make(cancelctx)
+	return ctx, func() { close(ctx) }
+}
+
+// cancelctx is the simplest possible cancellable context.
+type cancelctx (chan struct{})
+
+func (cancelctx) Deadline() (time.Time, bool) {
+	return time.Time{}, false
+}
+
+func (ctx cancelctx) Done() <-chan struct{} {
+	return ctx
+}
+
+func (ctx cancelctx) Err() error {
+	select {
+	case <-ctx:
+		return context.Canceled
+	default:
+		return nil
+	}
+}
+
+func (cancelctx) Value(key interface{}) interface{} {
+	return nil
+}
